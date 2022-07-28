034e43051166e988dc8061ace09b0921295304eb
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2022-07-28 15:16:36 -0300

    testing first song play, nice

--- songs/src/App.js
+++ songs/src/App.js
@@ -1,8 +1,34 @@
+import React, { useState } from "react";
+import spaceSong from './assets/space.mp3'
+import dutchie from './assets/dutchie.mp3'
 
 export default function App() {
+
+  const [song, setSong] = useState(dutchie);
+
+  const handleSong= () => {
+    if (song === spaceSong) {
+      setSong(dutchie)
+    }
+    else {
+      setSong(spaceSong)
+    }
+  }
+
   return (
     <>
-      <h1>Home</h1></>
+      <h1>Home</h1>
+      <audio controls>
+        <source src={song} type="audio/mpeg" />
+      </audio>
+      <h1>{song}</h1>
+      <div>
+        <button onClick={handleSong}>PLAY THE SONG, BABY!</button>
+      </div>
+
+
+    </>
+
 
   );
 }
