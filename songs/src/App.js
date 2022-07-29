03483d16015ee03e150a1006d5525ad928057f12
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2022-07-28 21:02:22 -0300

    play, pause, change song and volume functionalities

--- songs/src/App.js
+++ songs/src/App.js
@@ -1,35 +1,62 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import spaceSong from './assets/space.mp3'
 import dutchie from './assets/dutchie.mp3'
 
 export default function App() {
 
+  const audioPlayer = useRef()
   const [song, setSong] = useState(dutchie);
 
-  const handleSong= () => {
+  const handleSong = () => {
     if (song === spaceSong) {
       setSong(dutchie)
     }
     else {
       setSong(spaceSong)
     }
+
+    audioPlayer.current.load();
+    audioPlayer.current.play();
+  }
+  const handlePlay = () => {
+    if (audioPlayer.current.paused) {
+      audioPlayer.current.load();
+      audioPlayer.current.play();
+    } else {
+      audioPlayer.current.pause();
+    }
+  }
+
+  function handleVolume(option) {
+    if (option === 'aumentar' && audioPlayer.current.volume < 1) {
+      audioPlayer.current.volume += 0.2;
+
+    } else if (option === 'diminuir') {
+      audioPlayer.current.volume -= 0.2;
+
+    }
   }
 
   return (
     <>
-      <h1>Home</h1>
-      <audio controls>
-        <source src={song} type="audio/mpeg" />
-      </audio>
-      <h1>{song}</h1>
+      <h1>Music Player</h1>
       <div>
-        <button onClick={handleSong}>PLAY THE SONG, BABY!</button>
+        <h1>{song === dutchie ? 'Dutchie' : 'Space Song'}</h1>
+        <audio controls volume={0.5} ref={audioPlayer} >
+          <source src={song} type="audio/mpeg" />
+        </audio>
       </div>
-
-
+      <div>
+        <br />
+        <button onClick={handleSong}>MUDAR MUSICA</button>
+        <br />
+        <br />
+        <button onClick={handlePlay}>PLAY THE SONG</button>
+        <br />
+        <button onClick={() => handleVolume('aumentar')}>Aumentar volume</button>
+        <button onClick={() => handleVolume('diminuir')}>Diminuir volume</button>
+      </div>
     </>
-
-
   );
 }
 
